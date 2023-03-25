0d0231969f6afe2e36ff992da8f8f84f6019260c
Sanjay <sanjaysg.cse@gmail.com>
2023-03-25 19:28:36 +0530

    Create callApi.js

---
+++ callApi.js
@@ -0,0 +1,32 @@
+//https://stackoverflow.com/questions/60290580/how-do-i-make-an-api-call-using-createslice
+
+const usersSlice = createSlice({
+    name: "users",
+    initialState: {
+        loading: 'idle',
+        users: []
+    },
+    reducers: {
+        usersLoading(state, action) {
+            // Use a "state machine" approach for loading state instead of booleans
+            if(state.loading === 'idle') {
+                state.loading = 'pending'
+            }
+        },
+        usersReceived(state, action) {
+            if(state.loading === 'pending') {
+                state.loading = 'idle'
+                state.users = action.payload
+            }
+        }
+    }
+})
+
+const {usersLoading, usersReceived} = usersSlice.actions;
+
+const fetchUsers = () => async dispatch => {
+    dispatch(usersLoading());
+    const response = await usersAPI.fetchAll()
+    dispatch(usersReceived(response.data));
+
+}
